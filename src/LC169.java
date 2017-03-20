33e657d44b9ade62493adf1ea370f01904681de2
Rakesh Gupta <rakesh.gupta@salesforce.com>
2017-03-19 20:05:42 -0700

    added moore voting algorithm solution

--- src/LC169.java
+++ src/LC169.java
@@ -7,10 +7,29 @@ You may assume that the array is non-empty and the majority element always exist
  */
 public class LC169 {
     public static void main(String[] args) {
-        System.out.println(new LC169().majorityElement(new int[]{}));
+        System.out.println(new LC169().majorityElement(new int[] {}));
     }
-    public int majorityElement(int[] nums) {
+    
+    public int majorityElement1(int[] nums) {
         Arrays.sort(nums);
-        return nums[nums.length/2];
+        return nums[nums.length / 2];
+    }
+    
+    public int majorityElement(int[] nums) {
+        // field validations
+        int h = nums[0];
+        int c = 1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (c == 0) {
+                h = nums[i];
+                c = 1;
+            } else if (h == nums[i]) {
+                c++;
+            } else {
+                c--;
+            }
+        }
+        return h;
     }
 }
